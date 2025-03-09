9308f8298f28e0243091bcfdc25cd705448192b4
D-Abderrahmane <deabdou55@gmail.com>
2025-03-09 14:00:04 +0100

    fixing the window undefined

--- src/components/common/mouse-tracker/mouse-tracker-bg.tsx
+++ src/components/common/mouse-tracker/mouse-tracker-bg.tsx
@@ -3,9 +3,25 @@ import React, { useState, useEffect } from "react";
 
 const MouseTrackerBg = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [windowInnerWidth, setWindowInnerWidth] = useState(() => {
+    if (typeof window !== "undefined") {
+      return window.innerWidth;
+    }
+    return 800;
+  });
 
   useEffect(() => {
-    const size = window.innerWidth >= 1024 ? 800 : 480; // lg breakpoint is typically 1024px
+    function updateWindowInnerWidth() {
+      setWindowInnerWidth(window.innerWidth);
+    }
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", updateWindowInnerWidth);
+      return () => window.removeEventListener("resize", updateWindowInnerWidth);
+    }
+  }, []);
+
+  useEffect(() => {
+    const size = windowInnerWidth >= 1024 ? 800 : 480; // lg breakpoint is typically 1024px
     const halfSize = size / 2;
     const handleMouseMove = (e: MouseEvent) => {
       console.log("mouse move", e.clientX, e.clientY);
@@ -14,15 +30,15 @@ const MouseTrackerBg = () => {
         y: e.clientY - halfSize,
       });
     };
-
+    if (typeof window !== "undefined") {
+      window.addEventListener("mousemove", handleMouseMove);
+      return () => {
+        window.removeEventListener("mousemove", handleMouseMove);
+      };
+    }
     //window.addEventListener("wheel", handleMouseWheel);
-    window.addEventListener("mousemove", handleMouseMove);
     //window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, [window.innerWidth]);
+  }, [windowInnerWidth]);
 
   return (
     <div
